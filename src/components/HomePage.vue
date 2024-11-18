0871cf17951a2958e40632697797ccfc0e254170
Tini <christina.castedello@uni-bayreuth.de>
2024-11-18 17:26:26 +0100

    Erstellung HomePage.vue Dokument

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="homePage">
+      <h2>Willkommen auf der Home-Seite!</h2>
+      <p>Dies ist eine einfache Home-Komponente in Vue.js.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HomePage"
+  };
+  </script>
+  
+  <style scoped>
+  .homePage {
+    text-align: center;
+    margin-top: 20px;
+  }
+  </style>
