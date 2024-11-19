b0f4616f72bb71a9a5c0cb3e9b4847b6698874dd
Jakob-Woelfel <jakob.wolke@freenet.de>
2024-11-19 21:59:34 +0100

    Neue Maps Seite erstellt

---
+++ src/pages/MapsPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Maps Page</h1>
+      <p>This is the Maps subpage. Content will be added here later.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MapsPage',
+  };
+  </script>
+  
+  <style scoped>
+  @import '@/assets/css/base.css';
+  @import '@/assets/css/layout.css';
+  </style>
