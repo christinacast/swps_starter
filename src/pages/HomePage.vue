69d3fe83928025fc2fbfbe69abfef4c76be33b4b
Nikixs <singh222003@icloud.com>
2024-12-17 16:19:22 +0100

    home

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="homePage">
-      <SearchComponent />
-    </div>
-    <div class="main-content">
+  <div class="homePage">
+    <SearchComponent />
+  </div>
+  <div class="main-content">
+    <!-- "Wer fährt heute?" Block bleibt unverändert -->
     <div
       class="content-block"
       style="background-image: url('/images/icons8-traffic-jam-64.png')"
@@ -12,21 +13,23 @@
         <p>
           Hier kannst du einsehen wer heute alles fährt und für dich noch einen Platz frei hat
         </p>
-        <button>Learn More</button>
+        <button class="btn">Learn More</button>
       </div>
     </div>
 
+    <!-- "Meine Fahrten" Block bleibt unverändert -->
     <div
       class="content-block"
       style="background-image: url('/images/icons8-steering-wheel-64.png')"
     >
-      <div class="content-overlay">
+      <router-link to="/calendar" class="content-overlay">
         <h2>Meine Fahrten</h2>
         <p>Siehe deine anstehenden Fahrten ein</p>
-        <button>Learn More</button>
-      </div>
+        <button class="btn">Learn More</button>
+      </router-link>
     </div>
 
+    <!-- "Zu Uni Now" Block bleibt unverändert -->
     <div
       class="content-block"
       style="background-image: url('/images/icons8-university-64.png')"
@@ -34,24 +37,24 @@
       <div class="content-overlay">
         <h2>Zu Uni Now</h2>
         <p>Siehe deinen Uni Now Kalender ein</p>
-        <button>Learn More</button>
+        <button class="btn">Learn More</button>
       </div>
     </div>
   </div>
-  </template>
-  
-  <script>
-  import SearchComponent from '@/components/SearchComponent.vue';
+</template>
 
-  export default {
-    name: "HomePage", 
-    
-    components: {
-      SearchComponent,
-    }
-  };
-  </script>
+<script>
+import SearchComponent from '@/components/SearchComponent.vue';
+
+export default {
+  name: "HomePage", 
   
-  <style scoped>
-  @import '@/assets/css/pages/home.css';
-  </style>
+  components: {
+    SearchComponent,
+  }
+};
+</script>
+
+<style scoped>
+@import '@/assets/css/pages/home.css';
+</style>
