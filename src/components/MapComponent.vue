9ae06a7f322f233b9b21797844d73a6b757af50d
Jakob-Woelfel <jakob.wolke@freenet.de>
2025-02-04 14:34:11 +0100

    Clean up

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -8,7 +8,6 @@
 <script>
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { createClient } from '@supabase/supabase-js';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
@@ -53,7 +52,6 @@ export default {
       attribution: '&copy; OpenStreetMap contributors',
     }).addTo(this.map);
 
-    await this.loadMarkers();
     this.addRoute(); // Route hinzufügen
   },
   beforeUnmount() {
@@ -62,41 +60,6 @@ export default {
     }
   },
   methods: {
-    async loadMarkers() {
-      const supabaseUrl = 'https://ldpsaujnvjyjtflecpgb.supabase.co';
-      const supabaseKey = this.userPassword + 'OiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxkcHNhdWpudmp5anRmbGVjcGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NTc4NDcsImV4cCI6MjA0MTUzMzg0N30.56c2P7OPQyNd1flTA4vlyZ7Hn_8sFAWG8ThW6Q341DI';
-      const supabase = createClient(supabaseUrl, supabaseKey);
-
-      const { data: locations, error } = await supabase
-        .from('orte')
-        .select('ort_name, x_koordinate, y_koordinate, link');
-
-      if (error) {
-        console.error('Error fetching data from Supabase:', error);
-        return;
-      }
-
-      if (!locations || locations.length === 0) {
-        console.warn('No data found in the "orte" table');
-        return;
-      }
-
-      locations.forEach((location) => {
-        const { ort_name, x_koordinate, y_koordinate, link } = location;
-
-        if (x_koordinate && y_koordinate) {
-          const popupContent = `
-            <b>${ort_name}</b><br>
-            <a href="${link}" target="_blank">Website</a>
-          `;
-          L.marker([y_koordinate, x_koordinate])
-            .addTo(this.map)
-            .bindPopup(popupContent);
-        } else {
-          console.warn(`Invalid coordinates for ${ort_name}`);
-        }
-      });
-    },
     addRoute() {
       // Hier fügst du die Route hinzu (mit einer Polyline)
       const polyline = L.polyline(this.route, {
